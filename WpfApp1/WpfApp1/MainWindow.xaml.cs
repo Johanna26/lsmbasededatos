898dacf92b327bfbcf2524c4b278114974a46654
Johanna Garcia Nuñez <jgarcia162@ucol.mx>
2019-10-18 21:35:21 -0500

    potatoe

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Problemario1BaseDeDatos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OleDbConnection con;
+        DataTable dt;
+        public MainWindow()
+        {
+            InitializeComponent();
+            con = new OleDbConnection();
+            con.ConnectionString = "Provider=Microsoft.Jet.Oledb.4.0; Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\LasDivazasDB.mdb";
+            MostrarDatos();
+        }
+        private void MostrarDatos()
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Registro";
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+            gvDatos.ItemsSource = dt.AsDataView();
+            if (dt.Rows.Count > 0)
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Hidden;
+                gvDatos.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Visible;
+                gvDatos.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+        private void LimpiarTodo()
+        {
+            txtId.Text = "";
+            txtNombre.Text = "";
+            txtTelefono.Text = "";
+            txtDireccion.Text = "";
+            btnNuevo.Content = "Nuevo";
+            txtId.IsEnabled = true;
+        }
+
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+
+            cmd.Connection = con;
+        }
+
+        private void BtnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+                txtId.Text = row["Id"].ToString();
+                txtNombre.Text = row["Nombre"].ToString();
+                txtTelefono.Text = row["NumTel"].ToString();
+                txtDireccion.Text = row["Direccion"].ToString();
+                txtId.IsEnabled = false;
+                btnNuevo.Content = "Actualizar";
+            }
+            else
+            {
+                MessageBox.Show("Favor de seleccionar un Registro....");
+            }
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+                OleDbCommand cmd = new OleDbCommand();
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "delete from Registro where Id=" + row["Id"].ToString();
+                cmd.ExecuteNonQuery();
+                MostrarDatos();
+                MessageBox.Show("eliminado correctamente...");
+                LimpiarTodo();
+            }
+            else
+            {
+                MessageBox.Show("Favor de seleccionar un Registro...");
+            }
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            LimpiarTodo();
+        }
+
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
